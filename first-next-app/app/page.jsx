21fccadcc1c5b804455930021e8ea62704d2975a
NahidHasan131 <nahid7532140@gmail.com>
2026-04-15 10:50:02 +0600

    first next app

---
+++ first-next-app/app/page.jsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+       <p className="text-3xl font-semibold text-gray-300">hello world</p>
+      </main>
+    </div>
+  );
+}
